17a027e272e435d08f559acfe29698bce6e3d674
AinarFog <124941046+AinarFog@users.noreply.github.com>
2023-03-05 20:46:16 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,31 +1,31 @@
 # python3
-def right(n,l):
-  ind=[]
-  for i in range(n):
+def right(n,l,a):
+  ind=[-1]
+  for i in range(a):
     if(i>n):
       if (l[i] not in ind):
         ind.append(l[i])
-  return len(ind)
-
+  return (len(ind)-1)
   
 def left(n,l):
-  ind=[]
-  for i in range(n):
+  ind=[-1]
+  for i in range(n+1):
     if (l[i] not in ind):
       ind.append(l[i])
-  return len(ind)
+  return (len(ind)-1)
   
 def main():
   amount=int(input())
   nr=0
   if (amount>0 and amount<106):
+    list=[]
     for i in range(amount):
       n=int(input())
       if(n==-1):
         nr=i
       list.append(n)
-    if(right(nr,list)>=left(nr,list)):
-      print(right(nr,list))
+    if(right(nr,list,amount)>left(nr,list)):
+      print(right(nr,list,amount))
     else:
       print(left(nr,list))
 main()
