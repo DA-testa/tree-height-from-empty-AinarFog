74b456a7c0bc7983bcd4cde2026b6ae7655411c2
AinarFog <124941046+AinarFog@users.noreply.github.com>
2023-03-05 23:39:45 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,6 +6,26 @@ def right(n,l,a):
       if (l[i] not in ind):
         ind.append(l[i])
   return (len(ind))
+
+def links(l):
+  ind=[-1]
+  hed=l.index('-1')
+  for i in range(hed):
+    if(i!=hed):
+      if(l[i] not in ind):
+        ind.append(l[i])
+    else:
+      break
+  return(len(ind))
+
+def rechts(l):
+  ind=[-1]
+  hed=l.index('-1')
+  for i in range(len(l)):
+    if(i>hed):
+      if(l[i] not in ind):
+        ind.append(l[i])
+  return(len(ind))
   
 def left(n,l):
   ind=[-1]
@@ -13,8 +33,8 @@ def left(n,l):
     if (l[i] not in ind):
       ind.append(l[i])
   return (len(ind))
-  
-def main():
+
+def main1():
   amount=int(input())
   nr=0
   if (amount>0):
@@ -28,7 +48,23 @@ def main():
       print(right(nr,list,amount))
     else:
       print(left(nr,list))
+      
+def main2():
+  q2=input("file: ")
+  if ("a" not in q2):
+    with open(q2) as t:
+      data=t.readlines()
+      data.pop(0)
+      if(len(data)==1):
+        a=data[0]
+        step=a.split(" ")
+    if(rechts(step)>links(step)):
+      print(rechts(step))
+    else:
+      print(links(step))
 q1=input()
-q1=input()
+q1=input("Quest: ")
 if(q1=='I'):
-  main()
+  main1()
+elif (q1=='F'):
+  main2()
